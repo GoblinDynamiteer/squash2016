7d4e74881900deb9dc792b8eb8be5ffd9fae9b89
Johan Kämpe <kampe.johan@gmail.com>
2017-01-31 09:09:05 +0100

    General cleanup, added functions, startup

--- Code Examples/nanoboard_test/nanoboard_test.ino
+++ Code Examples/nanoboard_test/nanoboard_test.ino
@@ -1,5 +1,5 @@
 
-#include <SoftwareSerial.h>
+//#include <SoftwareSerial.h>
 #include <Adafruit_DotStar.h>
 
 #define NUMPIXELS 54
@@ -9,36 +9,44 @@
 #define MAX_STR 250
 #define VIBRATION_TRIGGER 10
 const int VIB_SENSOR_PIN = A0;
-int LEDBrightness = 10;
-#define LED_BRIGHTNESS_TRIGGER 255
+
+#define LED_BRIGHTNESS_MAX 255
 #define LED_BRIGHTNESS_HUM_MIN 10
 #define LED_BRIGHTNESS_HUM_MAX 20
 
 bool humRise = 0;
-int white = 0;
+bool on = 0;
 
-Adafruit_DotStar strip = Adafruit_DotStar(NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BRG);
+short ledGreen = 0;
+short ledRed = 0;
+short ledBlue = 0;
+short LEDBrightness = 10;
+
+Adafruit_DotStar strip = Adafruit_DotStar(
+    NUMPIXELS,
+    DATAPIN,
+    CLOCKPIN,
+    DOTSTAR_BRG
+  );
 
 void setup(){
-  Serial.begin(115200);
+  //Serial.begin(115200);
   #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000L)
-    clock_prescale_set(clock_div_1); // Enable 16 MHz on Trinket
+    clock_prescale_set(clock_div_1);
   #endif
 
-  strip.begin(); // Initialize pins for output
-  strip.show();  // Turn all LEDs off ASAP
+  strip.begin();
+  strip.show();
+  startUpLed();
 }
 
 void loop(){
   for(int i = 0; i < 54; i++){
-    strip.setPixelColor(i, white, 255, white);
+    strip.setPixelColor(i, ledGreen, ledRed, ledBlue);
   }
-  if(white > 0){
-    white--;
-  }
-  else{
-    white = 0;
-  }
+
+  fadeLedBlue();
+  fadeLedGreen();
 
   if(LEDBrightness > LED_BRIGHTNESS_HUM_MAX){
     LEDBrightness--;
@@ -57,19 +65,49 @@ void loop(){
   }
   strip.setBrightness(LEDBrightness);
   strip.show();
-  // Read Piezo ADC value in, and convert it to a voltage
   float vibrationData = (float)analogRead(VIB_SENSOR_PIN) / 1023.0 * 200.0;
   if(vibrationData > VIBRATION_TRIGGER){
     Serial.println("Triggar!");
-    white = 255;
+    setLedWhite();
     humRise = 0;
-    LEDBrightness = LED_BRIGHTNESS_TRIGGER;
+    LEDBrightness = LED_BRIGHTNESS_MAX;
     strip.setBrightness(LEDBrightness);
     for(int i = 0; i < 54; i++){
-      strip.setPixelColor(i, white, 255, white);
+      strip.setPixelColor(i, ledGreen, ledRed, ledBlue);
     }
     strip.show();
     delay(200);
   }
+}
 
+/*   Set led strip to white */
+void setLedWhite(void){
+  ledRed = ledBlue = ledGreen = 255;
+}
+
+/*    Decreases Blue color */
+void fadeLedBlue(void){
+  if(ledBlue){
+    ledBlue--;
+  }
+}
+
+/*    Decreases Green color */
+void fadeLedGreen(void){
+  if(ledGreen){
+    ledGreen--;
+  }
+}
+
+void startUpLed(void){
+  ledGreen = ledBlue = 0;
+  ledRed = LED_BRIGHTNESS_MAX;
+  short start = 0;
+  short end = NUMPIXELS;
+  while(start <= end){
+    strip.setPixelColor(start++, ledGreen, ledRed, ledBlue);
+    strip.setPixelColor(end--, ledGreen, ledRed, ledBlue);
+    strip.show();
+    delay(100);
+  }
 }
