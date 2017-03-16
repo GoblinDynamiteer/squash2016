deff7b221e6f5903a3dc2b955e2a6e77c34427d9
Johan Kämpe <kampe.johan@gmail.com>
2017-03-16 22:56:59 +0100

    New lightsaber code

--- Code/Code Main/lightsaber/lightsaber.ino
+++ Code/Code Main/lightsaber/lightsaber.ino
@@ -1,32 +1,58 @@
+/*   Squash-racket "lightup" mode
 
-#include <SoftwareSerial.h>
+      LED strip on racket light up at ball hit trigger.
+      Then fades to zero brightness.
+
+      For project Squash SM 2017
+      Members:
+      Dylan Saleh
+      Dennis Bunne
+      Johan Kämpe
+
+      Mölk Utbildning
+      Mjukvaruutvecklare inbyggda system
+
+      Code by Johan Kämpe
+ */
+
+#include <Arduino.h>
 #include <Adafruit_DotStar.h>
-#include <Adafruit_Soundboard.h>
 
 /*   Led-strip   */
-#define NUMPIXELS 54
+#define NUMPIXELS 50
+#define NUMCOLORS 6
 #define DATAPIN 4
 #define CLOCKPIN 5
-#define MAX_STR 250
-#define LED_BRIGHTNESS_MAX 255
-#define LED_BRIGHTNESS_HUM_MIN 10
-#define LED_BRIGHTNESS_HUM_MAX 20
-
-/*  Adafruit Audio FX pins  */
-#define SFX_TX 8
-#define SFX_RX 7
-#define SFX_RST 9
+#define LED_MAX_STR 250
+#define LED_BRIGHTNESS_MAX 200
+#define LED_BRIGHTNESS_MIN 0
 
 /*   Vibration sensor  */
 #define VIBRATION_TRIGGER 10
 const int VIB_SENSOR_PIN = A0;
 
-bool humRise = 0;
-bool on = 0;
-short ledGreen = 0;
-short ledRed = 255;
-short ledBlue = 0;
-short LEDBrightness = 10;
+/*   Delay between hit triggers  */
+#define HIT_DELAY 60
+#define FADE_DELAY 4
+
+/*   Predefined colors  */
+uint32_t ledColor[NUMCOLORS] = {
+  0x00FF00, 0x0000FF, 0xFF0000, 0xA5FF00,
+  0x00FFFF, 0xCCFFCC
+};
+
+/*  Indexes for ledColor array   */
+enum{RED, BLUE, GREEN, YELLOW, PURPLE, WHITE};
+
+#define CHOSENCOLOR GREEN
+
+uint32_t chosenColor = ledColor[CHOSENCOLOR];
+uint8_t brightness = LED_BRIGHTNESS_MIN;
+
+
+
+/*  Timers   */
+unsigned long timer = 0;
 
 /*   Init LED-strip as "strip"  */
 Adafruit_DotStar strip = Adafruit_DotStar(
@@ -36,115 +62,85 @@ Adafruit_DotStar strip = Adafruit_DotStar(
     DOTSTAR_BRG
   );
 
-/*   Software serial communication with Soundboard.  */
-SoftwareSerial ss = SoftwareSerial(SFX_TX, SFX_RX);
+void idleMode(void);
+void setLEDsOff(int count);
+bool sensorTrigger(void);
+bool checkHitDelay(void);
+void clashHit(void);
+void fadeMode();
 
-/* Init soundboard as "sfx"  */
-Adafruit_Soundboard sfx = Adafruit_Soundboard(
-  &ss,
-  NULL,
-  SFX_RST
-);
+/*  Sets pixel colors  */
+void setLEDs(int count, uint32_t color, uint8_t b){
+    setLEDsOff(count);
+    strip.setBrightness(b);
+    for(int i = 0; i < count && i < NUMPIXELS; i++){
+        strip.setPixelColor(i, color);
+    }
+}
 
-void fadeLedGreen(short fadeSpeed);
-void fadeRedGreen(short fadeSpeed);
-void fadeBlueGreen(short fadeSpeed);
-void setLedWhite(void);
-void clashLed(void);
-void startUpLed(void);
+/*   Turn off pixels  */
+void setLEDsOff(int count){
+    strip.setBrightness(0);
+    for(int i = 0; i < count && i < NUMPIXELS; i++){
+        strip.setPixelColor(i, 0, 0, 0);
+    }
+}
+
+void clashHit(void){
+  brightness = LED_BRIGHTNESS_MAX;
+  setLEDs(NUMPIXELS, chosenColor, brightness);
+  strip.show();
+}
 
 void setup(){
-  #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000L)
-    clock_prescale_set(clock_div_1);
-  #endif
-
-  /*    For communication with soundboard */
-  ss.begin(9600);
-  sfx.reset();
-
-  /*   For debug  */
-  Serial.begin(115200);
-
   /* Start Led-strip  */
   strip.begin();
-  //strip.show();
-  startUpLed();
+  /*  Start idle mode, ends if hit is detectled   */
+  idleMode();
+  clashHit();
+  timer = millis();
 }
 
 void loop(){
-  ledGreen = ledGreen > 0 ? ledGreen - 1 : 0;
-  ledBlue = ledBlue > 0 ? ledBlue - 1 : 0;
-  //ledRed = ledRed > 0 ? ledRed - 1 : 0;
-
-  /*  Set color of LED-strip   */
-  for(int i = 0; i < NUMPIXELS; i++){
-    strip.setPixelColor(i, ledGreen, ledRed, ledBlue);
+  /*   Vibration sensor detection    */
+  if(sensorTrigger() == 1 && checkHitDelay() == 1){
+    clashHit();
+    timer = millis();
   }
-
-  if(LEDBrightness > LED_BRIGHTNESS_HUM_MAX){
-    LEDBrightness -= 1;
+  if(brightness != LED_BRIGHTNESS_MIN){
+    fadeMode();
   }
-  else{
-    if(humRise == 1){
-      LEDBrightness += 1;
-      //erial.println("Brightn++");
-    }
-    else{
-      //Serial.println("Brightn--");
-      LEDBrightness -= 1;
-    }
-  }
-  if((LEDBrightness < LED_BRIGHTNESS_HUM_MIN ||
-    LEDBrightness > LED_BRIGHTNESS_HUM_MAX)){
-    humRise = !humRise;
-  }
-  strip.setBrightness(LEDBrightness);
-  strip.show();
-
-  float vibrationData =
-    (float)analogRead(VIB_SENSOR_PIN) * 0.2;
-  //Serial.print("Vibration Data: ");
-  //Serial.println(vibrationData);
-
-  if(vibrationData > VIBRATION_TRIGGER){
-    //Serial.println("Trigg!");
-    clashLed();
-    sfx.playTrack(2);
-    //delay(200);
-  }
-  /*  Do something smart to loop hum sound: */
-  /*  Modulus of ticker?   */
-  //sfx.playTrack(1);
 }
 
-/*   Set led strip to white */
-void setLedWhite(void){
-  /*  RGB at max values yields white color   */
-  ledRed = ledBlue = ledGreen = 255;
-}
-
-/* Set LED-strip to white at impact  */
-void clashLed(void){
-  setLedWhite();
-  humRise = 0;
-  LEDBrightness = LED_BRIGHTNESS_MAX;
-  strip.setBrightness(LEDBrightness);
-  for(int i = 0; i < NUMPIXELS; i++){
-    strip.setPixelColor(i, ledGreen, ledRed, ledBlue);
+void fadeMode(){
+  if(millis() % FADE_DELAY == 0){
+    brightness--;
+    if(brightness < LED_BRIGHTNESS_MIN){
+      brightness = LED_BRIGHTNESS_MIN;
+    }
+    setLEDs(NUMPIXELS, chosenColor, brightness);
+    strip.show();
   }
-  strip.show();
 }
 
 /*  LED-strip animation at startup   */
-void startUpLed(void){
-  ledGreen = ledBlue = 0;
-  ledRed = LED_BRIGHTNESS_MAX;
-  short start = 0;
-  short end = NUMPIXELS;
-  while(start <= end){
-    strip.setPixelColor(start++, ledGreen, ledRed, ledBlue);
-    strip.setPixelColor(end--, ledGreen, ledRed, ledBlue);
-    strip.show();
-    delay(20);
+void idleMode(void){
+  while(1){
+    if(sensorTrigger() == 1){
+      return;
+    }
   }
+}
+
+/*  Detects sensor trigger treshold   */
+bool sensorTrigger(void){
+  float vibrationData =
+    (float)analogRead(VIB_SENSOR_PIN) * 0.2;
+  return (vibrationData > VIBRATION_TRIGGER);
+}
+
+/*   Checks delay between hits  */
+bool checkHitDelay(void){
+  unsigned long offset = millis() - timer;
+  return (offset > HIT_DELAY);
 }
