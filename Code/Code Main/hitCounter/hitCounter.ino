8517a20f3baa607a333a07e9148477c97be4e70b
Johan Kämpe <kampe.johan@gmail.com>
2017-03-16 20:18:19 +0100

    Counts up to 200

--- Code/Code Main/hitCounter/hitCounter.ino
+++ Code/Code Main/hitCounter/hitCounter.ino
@@ -1,3 +1,6 @@
+/*   Squash-racket hit game  */
+
+#include <Arduino.h>
 #include <Adafruit_DotStar.h>
 
 /*   Led-strip   */
@@ -11,17 +14,24 @@
 
 /*   Vibration sensor  */
 #define VIBRATION_TRIGGER 10
-#define HIT_DELAY 50
 const int VIB_SENSOR_PIN = A0;
+
+/*   Delay between hit triggers  */
+#define HIT_DELAY 200
+
+/*   "Game" works for 60 seconds  */
+#define PLAY_TIME 60000
 
 bool idle = 1;
 int hits = 0;
-short ledGreen = LED_MAX_STR;
-short ledRed = LED_MAX_STR;
-short ledBlue = LED_MAX_STR;
 short LEDBrightness = 250;
 
-int hitDelay = HIT_DELAY;
+//GRB - Starts at RED
+uint32_t ledColor = 0x00FF00;
+
+unsigned long timer = 0;
+unsigned long startTime = 0;
+
 
 /*   Init LED-strip as "strip"  */
 Adafruit_DotStar strip = Adafruit_DotStar(
@@ -32,43 +42,42 @@ Adafruit_DotStar strip = Adafruit_DotStar(
   );
 
 void startUpLed(void);
+void setLEDsOff(int count);
+bool sensorTrigger(void);
+bool checkHitDelay(void);
 
 void setLEDs(int count){
+    setLEDsOff(count);
     strip.setBrightness(LED_BRIGHTNESS_MAX);
-    for(int i = 0; i < count; i++){
-        strip.setPixelColor(i, ledGreen, ledRed, ledBlue);
+    for(int i = 0; i < count && i < NUMPIXELS; i++){
+        strip.setPixelColor(i, ledColor);
     }
-    strip.show();
 }
 
-void setLEDsOff(void){
+void setLEDsOff(int count){
     strip.setBrightness(0);
-    for(int i = 0; i < NUMPIXELS; i++){
+    for(int i = 0; i < count && i < NUMPIXELS; i++){
         strip.setPixelColor(i, 0, 0, 0);
     }
-    strip.show();
 }
 
 void setLEDCount(int hits){
-    if(hits > 0)){
-        ledGreen = LED_MAX_STR;
-        ledBlue = 0;
-        ledRed = 0;
-        setLEDs(hits);
+    if(hits > 0){
+      ledColor = 0xFF0000; //GREEN
+      setLEDs(hits);
     }
     if(hits > NUMPIXELS){
-        ledGreen = 0;
-        ledBlue = LED_MAX_STR;
-        ledRed = 0;
-        setLEDs(hits - NUMPIXELS);
+      ledColor = 0x0000FF; //BLUE
+      setLEDs(hits - NUMPIXELS);
     }
-    if(hits > NUMPIXELS * 2){
-        ledGreen = 0;
-        ledBlue = 0;
-        ledRed = LED_MAX_STR;
-        setLEDs(hits - (NUMPIXELS * 2));
+    if(hits > (NUMPIXELS * 2)){
+      ledColor = 0x00FF00; //RED
+      setLEDs(hits - (NUMPIXELS * 2));
     }
-    return;
+    if(hits > (NUMPIXELS * 3)){
+      ledColor = 0xA5FF00; //ORANGE rgb(255,165,0) 	#FFA500
+      setLEDs(hits - (NUMPIXELS * 3));
+    }
 }
 
 void setup(){
@@ -80,29 +89,33 @@ void setup(){
 
 
 void loop(){
-    hitDelay--;
-  /*  Set color of LED-strip   */
-  setLEDsOff();
-  setLEDCount(hits);
   /*   Vibration sensor detection    */
-  float vibrationData =
-    (float)analogRead(VIB_SENSOR_PIN) * 0.2;
-  if((vibrationData > VIBRATION_TRIGGER) && (hitDelay < 0)){
-    hits++;
-    hitDelay = HIT_DELAY;
+  if(sensorTrigger() == 1 && checkHitDelay() == 1){
+    setLEDCount(hits++);
+    strip.show();
+    timer = millis();
   }
 }
 
 /*  LED-strip animation at startup   */
 void startUpLed(void){
-  ledGreen = ledBlue = 0;
-  ledRed = LED_MAX_STR;
   short start = 0;
   short end = NUMPIXELS;
   while(start <= end){
-    strip.setPixelColor(start++, ledGreen, ledRed, ledBlue);
-    strip.setPixelColor(end--, ledGreen, ledRed, ledBlue);
+    strip.setPixelColor(start++, ledColor);
+    strip.setPixelColor(end--, ledColor);
     strip.show();
     delay(20);
   }
+}
+
+bool sensorTrigger(void){
+  float vibrationData =
+    (float)analogRead(VIB_SENSOR_PIN) * 0.2;
+  return (vibrationData > VIBRATION_TRIGGER);
+}
+
+bool checkHitDelay(void){
+  unsigned long offset = millis() - timer;
+  return (offset > HIT_DELAY);
 }
